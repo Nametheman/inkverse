b1cb8e85b203d06fa4ea309561f0aec064c0f91f
nametheman <owoemma59@gmail.com>
2024-08-13 16:59:28 +0100

    first commit

---
+++ src/utils/apiUrls.ts
@@ -0,0 +1,5 @@
+const baseURL = import.meta.env.VITE_REACT_APP_BASE_URL;
+
+export const INKVERSE_URLS = {
+  BASE_URL: baseURL,
+};
---
+++ src/utils/axios.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosInstance } from "axios";
+import { INKVERSE_URLS } from "./apiUrls";
+
+const InkverseAPI = axios.create({
+  baseURL: INKVERSE_URLS.BASE_URL,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+InkverseAPI.interceptors.request.use(
+  (config) => {
+    const key = import.meta.env.VITE_REACT_BOOK_API_KEY;
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default InkverseAPI as AxiosInstance;
